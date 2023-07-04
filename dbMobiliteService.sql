2372b45c8bfdf2421942f6a39af03f8885ef8897
Douglas Cordeiro Marques <dodo-douglas-1@hotmail.com>
2023-07-04 04:35:48 -0300

    FINAL

--- dbMobiliteService.sql
+++ dbMobiliteService.sql
@@ -47,7 +47,25 @@ cidade varchar(100),
 estado char(2),
 primary key(codCli));
 
+create table tbServicos(
+codServ int not null auto_increment,
+descricao varchar(100),
+tipo int default 0 check(tipo >=0),
+valor decimal(9,2) default 0 check(valor >=0),
+primary key(codServ));
+
+create table tbChamados(
+codCham int not null auto_increment,
+codCli int not null,
+codFunc int not null,
+codServ int not null,
+primary key(codCham),
+foreign key(codCli) references tbClientes(clodCli),
+foreign key(codFunc) references tbFuncionarios(codFunc),
+foreign key(codServ) references tbServicos(codServ));
+
 insert into tbFuncionarios (nome,email,cpf,telefone,sexo,endereco,numero,cep,bairro,cidade,estado) values ('felipe gomes', 'felipe.gomessan@gmail.com','21342-344','(34) 54446-3535','masculino','rua cristal',233,32432-545,'canduva','ribeirinho','bh');
 insert into tbClientes (nome,email,cpf,telefone,sexo,endereco,numero,cep,bairro,cidade,estado) values ('felipe gomes', 'felipe.gomessan@gmail.com','21342-344','(34) 54446-3535','masculino','rua cristal',233,32432-545,'canduva','ribeirinho','bh');
 insert into tbUsuarios (nomeUsu, senhaUsu) values ('felipe@gomes','123456xs');
+insert into tbServicos (descricao, tipo, valor) values ('Suporte do tipo N1', '1', 'à combinar');
 
